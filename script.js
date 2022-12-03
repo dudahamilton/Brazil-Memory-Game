d01940182fbb0a2e04472f7db7fb3dc21f7065da
dudahamilton <mariaehamilton28@gmail.com>
2022-12-02 21:42:41 -0800

    javascripts steps

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Brasil HEXA campeao");
+/* console.log("Brasil HEXA campeao"); */
+ /* Javascript steps:
+ 1- add event listerns to the cards
+ 2- make the flip once clicked
+ 3-not possible to click in the same pic twice in a row
+ 4- switch player every two cards clickes 
+ 5- flip last two cards once hide button was clicked
+ 6- count pair matches
+ 7- winning conditionals
+ 8- restart button unflips all cards at once and restart players count
+ 9- display winning message*/
