50b2764e3af415afeb6c02ac10aad21c39bdaa08
dudahamilton <mariaehamilton28@gmail.com>
2022-12-04 21:28:52 -0800

    first image flips onclick

--- script.js
+++ script.js
@@ -9,3 +9,13 @@
  7- winning conditionals
  8- restart button unflips all cards at once and restart players count
  9- display winning message*/
+
+function flipImg() {
+    let image_click = document.getElementById('pic1')
+    if (image_click.src.match('brazil.jpg')){
+        image_click.src = "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/43/ac/8f/pelourinho.jpg?w=1200&h=-1&s=1"
+    } else{
+        image_click.src = 'brazil.jpg'
+    }
+}
+
