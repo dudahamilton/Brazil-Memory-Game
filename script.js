f199f3b3d69dce1985aec16c2c3ac6166d5b1964
dudahamilton <mariaehamilton28@gmail.com>
2022-12-06 14:22:12 -0800

    start of the game function, some variables are assigned, the array of cards is inside the function and HTML has been changed

--- script.js
+++ script.js
@@ -12,119 +12,56 @@
 
  // Flipping images (is away too repetitive ask for help)
 
-/* function flipImg() {
-    let imageClick = document.getElementById('pic1')
-    if (imageClick.src.match('brazil.jpg')){
-        imageClick.src = "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/43/ac/8f/pelourinho.jpg?w=1200&h=-1&s=1"
-    } else{
-        imageClick.src = 'brazil.jpg'
-    }
-}
 
-function flipImg2() {
-    let imageClick = document.getElementById('pic2')
-    if (imageClick.src.match('brazil.jpg')){
-        imageClick.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTe44SuPVamIj0yVSNv6KQkV0YXcSnEyVXo4w&usqp=CAU"
-    } else{
-        imageClick.src = 'brazil.jpg'
-    }
-}
-
- function flipImg3() {
-    let imageClick = document.getElementById('pic3')
-    if (imageClick.src.match('brazil.jpg')){
-        imageClick.src = "https://www.traveller.com.au/content/dam/images/h/1/q/d/y/0/image.related.articleLeadwide.620x349.h1qdxw.png/1598825371635.jpg"
-    } else{
-        imageClick.src = 'brazil.jpg'
-    }
-}
-function flipImg4() {
-    let imageClick = document.getElementById('pic4')
-    if (imageClick.src.match('brazil.jpg')){
-        imageClick.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3QBHXWOLqJzAD3Qn9SLviVdexUFJCNnfiWw&usqp=CAU"
-    } else{
-        imageClick.src = 'brazil.jpg'
-    }
-}
-
-function flipImg5() {
-    let imageClick = document.getElementById('pic5')
-    if (imageClick.src.match('brazil.jpg')){
-        imageClick.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTY_vZt65OuTgT6kjMhKEYNwGXoxj3or2AcVA&usqp=CAU"
-    } else{
-        imageClick.src = 'brazil.jpg'
-    }
-}
-
-function flipImg6() {
-    let imageClick = document.getElementById('pic6')
-    if (imageClick.src.match('brazil.jpg')){
-        imageClick.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTY_vZt65OuTgT6kjMhKEYNwGXoxj3or2AcVA&usqp=CAU"
-    } else{
-        imageClick.src = 'brazil.jpg'
-    }
-}
-
-function flipImg7() {
-    let imageClick = document.getElementById('pic7')
-    if (imageClick.src.match('brazil.jpg')){
-        imageClick.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3QBHXWOLqJzAD3Qn9SLviVdexUFJCNnfiWw&usqp=CAU"
-    } else{
-        imageClick.src = 'brazil.jpg'
-    }
-}
-
-function flipImg8() {
-    let imageClick = document.getElementById('pic8')
-    if (imageClick.src.match('brazil.jpg')){
-        imageClick.src = "https://www.traveller.com.au/content/dam/images/h/1/q/d/y/0/image.related.articleLeadwide.620x349.h1qdxw.png/1598825371635.jpg"
-    } else{
-        imageClick.src = 'brazil.jpg'
-    }
-}
-function flipImg9() {
-    let imageClick = document.getElementById('pic9')
-    if (imageClick.src.match('brazil.jpg')){
-        imageClick.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTe44SuPVamIj0yVSNv6KQkV0YXcSnEyVXo4w&usqp=CAU"
-    } else{
-        imageClick.src = 'brazil.jpg'
-    }
-}
-
-function flipImg10() {
-    let imageClick = document.getElementById('pic10')
-    if (imageClick.src.match('brazil.jpg')){
-        imageClick.src = "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/43/ac/8f/pelourinho.jpg?w=1200&h=-1&s=1"
-    } else{
-        imageClick.src = 'brazil.jpg'
-    }
-}
- */
-
-// cities array
-let cardArray = [ 
-    { city: "salvador", img: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/43/ac/8f/pelourinho.jpg?w=1200&h=-1&s=1", },
-    { city: "salvador", img: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/43/ac/8f/pelourinho.jpg?w=1200&h=-1&s=1", },  
-    { city: "rio", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTe44SuPVamIj0yVSNv6KQkV0YXcSnEyVXo4w&usqp=CAU", },
-    { city: "rio", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTe44SuPVamIj0yVSNv6KQkV0YXcSnEyVXo4w&usqp=CAU", },
-    { name: "noronha", img: "https://www.traveller.com.au/content/dam/images/h/1/q/d/y/0/image.related.articleLeadwide.620x349.h1qdxw.png/1598825371635.jpg", },
-    { name: "noronha", img: "https://www.traveller.com.au/content/dam/images/h/1/q/d/y/0/image.related.articleLeadwide.620x349.h1qdxw.png/1598825371635.jpg", },
-    { name: "bonito", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3QBHXWOLqJzAD3Qn9SLviVdexUFJCNnfiWw&usqp=CAU", }, 
-    { name: "bonito", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3QBHXWOLqJzAD3Qn9SLviVdexUFJCNnfiWw&usqp=CAU", }, 
-    { name: "jalapao", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTY_vZt65OuTgT6kjMhKEYNwGXoxj3or2AcVA&usqp=CAU", },
-    { name: "jalapao", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTY_vZt65OuTgT6kjMhKEYNwGXoxj3or2AcVA&usqp=CAU", },
-    ]; 
     
+const gameActive = (function(){
+    // grabbing the game container from html
+    const container = document.getElementById('game-container');
+    // stablishing number of rows and columns inside the big container
+    const row = 2;
+    const column = 5;
+    // devinding the space of the container between the images boxes
+    const boxWidth = container.offsetWidth /column;
+    const boxHeight = container.offsetHeight / row;
+    // array holding the images 
+    let cardArray = [ 
+        { city: "salvador", img: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/43/ac/8f/pelourinho.jpg?w=1200&h=-1&s=1", },
+        { city: "salvador", img: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/43/ac/8f/pelourinho.jpg?w=1200&h=-1&s=1", },  
+        { city: "rio", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTe44SuPVamIj0yVSNv6KQkV0YXcSnEyVXo4w&usqp=CAU", },
+        { city: "rio", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTe44SuPVamIj0yVSNv6KQkV0YXcSnEyVXo4w&usqp=CAU", },
+        { city: "noronha", img: "https://www.traveller.com.au/content/dam/images/h/1/q/d/y/0/image.related.articleLeadwide.620x349.h1qdxw.png/1598825371635.jpg", },
+        { city: "noronha", img: "https://www.traveller.com.au/content/dam/images/h/1/q/d/y/0/image.related.articleLeadwide.620x349.h1qdxw.png/1598825371635.jpg", },
+        { city: "bonito", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3QBHXWOLqJzAD3Qn9SLviVdexUFJCNnfiWw&usqp=CAU", }, 
+        { city: "bonito", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3QBHXWOLqJzAD3Qn9SLviVdexUFJCNnfiWw&usqp=CAU", }, 
+        { city: "jalapao", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTY_vZt65OuTgT6kjMhKEYNwGXoxj3or2AcVA&usqp=CAU", },
+        { city: "jalapao", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTY_vZt65OuTgT6kjMhKEYNwGXoxj3or2AcVA&usqp=CAU", },
+        ];
+    
+    
+
+}
+
+)
+
+
+
+
+
+
+
+
+
+
 
 // Storing event listeners in variables
 
 // changing the base image to a city once clicked
-function flipCards(){
+/* function flipCards(){
     // once image is clicked src changes to array img
 }
-/* 
+/*  */
 
-playerOneTurn is true if player has clicked in less than 2 images
+/* playerOneTurn is true if player has clicked in less than 2 images
 
     matchFunction => once player clicked in 2 verify if images src match{
 if match add 1 to playerOnePairs(it starts at 0) and remove eventListener
@@ -148,7 +85,7 @@ winning connditions: if (playerOnePairs >= 3){
 }
 
  */
-
+ 
 
 
 
