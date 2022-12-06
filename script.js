1fa831263dd4ae0a85bba15b7d95e2d01b141f19
dudahamilton <mariaehamilton28@gmail.com>
2022-12-06 12:27:04 -0800

    card array and new pseudo code

--- script.js
+++ script.js
@@ -12,7 +12,7 @@
 
  // Flipping images (is away too repetitive ask for help)
 
-function flipImg() {
+/* function flipImg() {
     let imageClick = document.getElementById('pic1')
     if (imageClick.src.match('brazil.jpg')){
         imageClick.src = "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/43/ac/8f/pelourinho.jpg?w=1200&h=-1&s=1"
@@ -99,6 +99,63 @@ function flipImg10() {
         imageClick.src = 'brazil.jpg'
     }
 }
+ */
+
+// cities array
+let cardArray = [ 
+    { city: "salvador", img: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/43/ac/8f/pelourinho.jpg?w=1200&h=-1&s=1", },
+    { city: "salvador", img: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/43/ac/8f/pelourinho.jpg?w=1200&h=-1&s=1", },  
+    { city: "rio", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTe44SuPVamIj0yVSNv6KQkV0YXcSnEyVXo4w&usqp=CAU", },
+    { city: "rio", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTe44SuPVamIj0yVSNv6KQkV0YXcSnEyVXo4w&usqp=CAU", },
+    { name: "noronha", img: "https://www.traveller.com.au/content/dam/images/h/1/q/d/y/0/image.related.articleLeadwide.620x349.h1qdxw.png/1598825371635.jpg", },
+    { name: "noronha", img: "https://www.traveller.com.au/content/dam/images/h/1/q/d/y/0/image.related.articleLeadwide.620x349.h1qdxw.png/1598825371635.jpg", },
+    { name: "bonito", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3QBHXWOLqJzAD3Qn9SLviVdexUFJCNnfiWw&usqp=CAU", }, 
+    { name: "bonito", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3QBHXWOLqJzAD3Qn9SLviVdexUFJCNnfiWw&usqp=CAU", }, 
+    { name: "jalapao", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTY_vZt65OuTgT6kjMhKEYNwGXoxj3or2AcVA&usqp=CAU", },
+    { name: "jalapao", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTY_vZt65OuTgT6kjMhKEYNwGXoxj3or2AcVA&usqp=CAU", },
+    ]; 
+    
+
+// Storing event listeners in variables
+
+// changing the base image to a city once clicked
+function flipCards(){
+    // once image is clicked src changes to array img
+}
+/* 
+
+playerOneTurn is true if player has clicked in less than 2 images
+
+    matchFunction => once player clicked in 2 verify if images src match{
+if match add 1 to playerOnePairs(it starts at 0) and remove eventListener
+else unflip card after 5 seconds
+}
+playerOnePairs{
+    starts at zero and adds 1 everytime match is true
+    this probably should go inside matcFunction
+    returns pairs
+}
+
+
+fliping turn between players 
+
+
+gameOver: once winning conditions are met
+winning connditions: if (playerOnePairs >= 3){
+    innertext player 1 won
+} else if (playerTwoPairs >= 3)
+    innertext player 2 won
+}
+
+ */
+
+
+
+
+
+
+
+
 
 
 // restart button
